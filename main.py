e472b763da3d2437926e1b8e8059d62243ebdb6f
gfavalessa <gfavalessaf@gmail.com>
2018-03-27 23:26:54 -0300

    Main last version - running geiger_counter_cloud

--- main.py
+++ main.py
@@ -1,51 +1,36 @@
-# The MIT License (MIT)
-# Copyright (c) 2017 Mike Teachman
-# https://opensource.org/licenses/MIT
+#   MicroPython Geiger Counter Main Code for Esp32 board (ThingSpeak IoT Cloud)
 #
-# Publish data to a Thingspeak channel using the MQTT protocol
+#   This code interacts with the RadiationD-v1.1 (CAJOE) Geiger counter board
+#   and reports readings in CPM (Counts Per Minute), showing the  information
+#   in a oled display and send the data to ThingSpeak IoT Cloud (MQTT).
 #
-# Micropython implementation using the ESP8266 platform
-# Tested using Micropython v1.9.3 (Nov 1, 2017)
-#
-# Tested using Hardware:
-# - Adafruit Feather HUZZAH ESP8266
-#
-# prerequisites:
-# - Thingspeak account
-# - Thingspeak channel to publish data
-# - Thinkspeak Write API Key for the channel
-# - Thinkspeak MQTT API Key for the account
-#
-#   Autor: Gabriel F. Fraga
+#   Author: Gabriel F. Fraga
+#   27 Mar 2018
 
+import utime
+import machine
+import ssd1306
 import network
 from umqtt.robust import MQTTClient
-import utime
 import uos
 import gc
+from machine import Pin
 
-#
+
 # WiFi connection information
-#
-wifiSSID = "batnet"  # EDIT - enter name of WiFi connection point
-wifiPassword = "naotemsenha"  # EDIT - enter WiFi password
+wifiSSID = "batnet"             # EDIT - enter name of WiFi connection point
+wifiPassword = "naotemsenha"    # EDIT - enter WiFi password
 
-#
-# turn off the WiFi Access Point
-#
+# Turn off the WiFi Access Point
 ap_if = network.WLAN(network.AP_IF)
 ap_if.active(False)
 
-#
-#   connect the ESP8266 device to the WiFi network
-#
+# Connect the ESP32 device to the WiFi network
 wifi = network.WLAN(network.STA_IF)
 wifi.active(True)
 wifi.connect(wifiSSID, wifiPassword)
 
-#
-# wait until the ESP8266 is connected to the WiFi network
-#
+# Wait until the ESP32 is connected to the WiFi network
 maxAttempts = 20
 attemptCount = 0
 while not wifi.isconnected() and attemptCount < maxAttempts:
@@ -53,15 +38,12 @@ while not wifi.isconnected() and attemptCount < maxAttempts:
     utime.sleep(1)
     print('did not connect...trying again')
 
-#
-# create a random MQTT clientID
-#
+# Create a random MQTT clientID
 randomNum = int.from_bytes(uos.urandom(3), 'little')
 myMqttClient = bytes("client_" + str(randomNum), 'utf-8')
 
-
-# connect to Thingspeak MQTT broker
-# connection uses unsecure TCP (port 1883)
+# Connect to Thingspeak MQTT broker
+# Connection uses unsecure TCP (port 1883)
 #
 # Steps to change to a secure connection (encrypted) using TLS
 #   a) change port below to "port=8883
@@ -80,17 +62,54 @@ client = MQTTClient(client_id=myMqttClient,
 
 client.connect()
 
-#
-# publish free heap to Thingspeak using MQTT
-#
-thingspeakChannelId = b"442333"  # EDIT - enter Thingspeak Channel ID
+# Publish free heap to Thingspeak using MQTT
+thingspeakChannelId = b"442333"                     # EDIT - enter Thingspeak Channel ID
 thingspeakChannelWriteApiKey = b"DYUTUVNWIG514ICS"  # EDIT - enter Thingspeak Write API Key
 publishPeriodInSec = 30
+
+
+# Variables used in GeigerCounter algorithm
+LOG_PERIOD = 30000
+MAX_PERIOD = 60000
+multiplier = MAX_PERIOD / LOG_PERIOD
+previousMillis = 0
+cpm = 0
+interruptCounter = 0
+
+# Callback function for interrupt counter
+def counter_callback(pin):
+    global interruptCounter
+    interruptCounter = interruptCounter + 1
+
+# Setup the oled
+#i2c = machine.I2C(scl=machine.Pin(22), sda=machine.Pin(21))
+#oled = ssd1306.SSD1306_I2C(128, 64, i2c)
+#oled.fill(0)
+#oled.text('Welcome', 30, 20)
+#oled.show()
+
+# Setup interrupt
+p0 = Pin(34, Pin.IN)
+p0.irq(trigger=Pin.IRQ_FALLING, handler=counter_callback)
+
+
+# Program Main Loop
 while True:
+    # Display cpm counter
+    cpm = interruptCounter * multiplier
+    print("CPM:", cpm)
+    #    oled.fill(0)
+    #oled.text('Geiger Counter', 10, 0)
+    #oled.text('CPM: ', 30, 20)
+    #oled.text(str(cpm), 70, 20)
+    #oled.show()
+    interruptCounter = 0
+
     freeHeapInBytes = gc.mem_free()
     credentials = bytes("channels/{:s}/publish/{:s}".format(thingspeakChannelId, thingspeakChannelWriteApiKey), 'utf-8')
-    payload = bytes("field1={:.1f}\n".format(freeHeapInBytes), 'utf-8')
+    payload = bytes("field1={:.2f}\n".format(cpm), 'utf-8')
     client.publish(credentials, payload)
     utime.sleep(publishPeriodInSec)
 
 client.disconnect()
+
